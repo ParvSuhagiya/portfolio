20cdf3fa981e2ca7a33542aa0dae41c4f824bc36
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2026-04-06 22:30:11 +0530

    added location

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -30,7 +30,7 @@ const AboutMe = ({ words }) => {
             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
             <circle cx="12" cy="10" r="3"></circle>
           </svg>
-          India /* Update with actual location */
+          Ahmedabad,Gujarat,India
         </div>
         
         <div className="about-links">
